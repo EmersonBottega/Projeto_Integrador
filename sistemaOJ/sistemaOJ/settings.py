cdeb2cce9686ca709e9b927e9c7e7727b5886e23
nicoleinae <nicoleinae@gmail.com>
2024-10-16 23:08:12 -0300

    feat: banco de dados

--- sistemaOJ/sistemaOJ/settings.py
+++ sistemaOJ/sistemaOJ/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework'
-    'appOJ'
+    'rest_framework',
+    'appOJ',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
